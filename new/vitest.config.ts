06c072ac5f78b798db2362fd86ac933760d555e5
miy <miy1924@gmail.com>
2026-04-16 09:26:23 +0900

    fix package.json, tsconfig.json

--- new/vitest.config.ts
+++ new/vitest.config.ts
@@ -1,8 +1,15 @@
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "node",
     include: ["packages/*/test/**/*.test.ts"],
+    alias: {
+      "@mei-friend/core": path.resolve(
+        __dirname,
+        "./packages/core/src/index.ts",
+      ),
+    },
   },
 });
